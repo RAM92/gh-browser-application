2390690b8a456bc93172d32f78549c89cc64cc68
RAM92 <RAM92@users.noreply.github.com>
2016-02-26 21:26:26 +0000

    Alter User service to fetch repos after user details
    -Fetch user's repos after getting user detailss
    -Add this to the data for the user and return in promise

--- src/app/user/user.service.js
+++ src/app/user/user.service.js
@@ -6,10 +6,18 @@
     .service('ghUser', userService);
 
   /** @ngInject */
-  function userService(Restangular) {
+  function userService($log, Restangular) {
       return function (username) {
           username = username || 'RAM92'; //Default to me
-          return Restangular.one('users', username).get();
+          return Restangular.one('users', username).get().then(function (user) {
+              $log.log('Successfully got details for ' + username);
+              var repoUrl = user.repos_url;
+              return user.allUrl('repos', repoUrl).getList().then(function (repos) {
+                  $log.log('Successfully got repos for ' + username);
+                  user.repos = repos;
+                  return user;
+              });
+          });
       };
   }
 
