7a13eac3230928bf2dbbd8ac668c52dcc690e723
RAM92 <RAM92@users.noreply.github.com>
2016-02-27 20:18:53 +0000

    Remove navbar, put username in route

--- src/app/user/user.route.js
+++ src/app/user/user.route.js
@@ -9,15 +9,15 @@
   function routerConfig($stateProvider) {
     $stateProvider
       .state('user', {
-        url: '/user/',
+        url: '/user/:username',
         templateUrl: 'app/user/user.html',
         controller: 'UserController',
         controllerAs: 'user',
         resolve: {
           
           /** @ngInject */
-          data: function (ghUser) {
-            return ghUser();
+          data: function (ghUser, $stateParams) {
+            return ghUser($stateParams.username);
           }
         }
       });
